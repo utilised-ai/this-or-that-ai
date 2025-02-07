20115224c0bf597ac0f9681089f06f475d2fb4a2
utilised AI <bob@utilised.ai>
2025-02-07 21:32:25 +0800

    Create Gruntfile.js
    Added Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function (grunt) {
+  // Project configuration
+  grunt.initConfig({
+    // e.g. a simple “lint” or “uglify” task config
+  });
+
+  // Load any grunt plugins your tasks need:
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Register some default tasks
+  grunt.registerTask('default', []);
+};
